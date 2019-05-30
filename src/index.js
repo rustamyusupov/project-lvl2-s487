55ccf0400c0aad63ad37209d5cdd58233c27f329
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-05-30 21:18:15 +0200

    feat(index): add help information

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-export default () => console.log('init!');
+const program = require('commander');
+
+export default () => {
+  program
+    .version('0.1.0')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-V, --version', 'output the version number')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((cmd, env) => {
+      console.log(cmd);
+      console.log(env);
+    });
+
+  program.parse(process.argv);
+};
