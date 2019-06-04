36ea9cdd65c7fad728da3123e09ffe24d6ccbd2e
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-06-04 20:52:25 +0200

    refactor(src): export lib as package, move lib to diff, parse args in bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
-import run from '..';
+
+import program from 'commander';
+
+import { version } from '../../package.json';
+import diff from '..';
+
+const run = () => {
+  program
+    .description('Compares two configuration files and shows a difference.')
+    .version(version)
+    .option('-V, --version', 'output the version number')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((cmd, env) => {
+      console.log(cmd);
+      console.log(env);
+      diff();
+    });
+
+  program.parse(process.argv);
+};
 
 run();
--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-import { version } from '../package.json';
+import diff from './diff';
 
-const program = require('commander');
-
-export default () => {
-  program
-    .description('Compares two configuration files and shows a difference.')
-    .version(version)
-    .option('-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format')
-    .arguments('<firstConfig> <secondConfig>')
-    .action((cmd, env) => {
-      console.log(cmd);
-      console.log(env);
-    });
-
-  program.parse(process.argv);
-};
+export default diff;
