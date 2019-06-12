2892ac368aeec90660a0f3fe3116f7e76d0484ed
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-06-12 21:00:23 +0200

    test(tests): refactor, add folder for fixtures paths

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -3,10 +3,10 @@ import { readFile } from '../src/parsers';
 
 const paths = ['json', 'yaml', 'ini'].map(ext => [
   ext,
-  `__tests__/__fixtures__/before.${ext}`,
-  `__tests__/__fixtures__/after.${ext}`,
+  `__tests__/__fixtures__/${ext}/before.${ext}`,
+  `__tests__/__fixtures__/${ext}/after.${ext}`,
 ]);
 
 test.each(paths)('compare %s files', (_, firstConfig, secondConfig) => {
-  expect(diff(firstConfig, secondConfig)).toBe(readFile('__tests__/__fixtures__/result.txt'));
+  expect(diff(firstConfig, secondConfig)).toBe(readFile('__tests__/__fixtures__/result/result.txt'));
 });
--- __tests__/parsers.test.js
+++ __tests__/parsers.test.js
@@ -2,13 +2,6 @@ import _ from 'lodash/fp';
 
 import parser from '../src/parsers';
 
-test('parsers', () => {
-  const jsonFile = '__tests__/__fixtures__/before.json';
-  const yamlFile = '__tests__/__fixtures__/after.yaml';
+const paths = ['json', 'yaml', 'ini'].map(ext => [ext, `__tests__/__fixtures__/${ext}/before.${ext}`]);
 
-  const jsonData = parser(jsonFile);
-  const yamlData = parser(yamlFile);
-
-  expect(_.isObject(jsonData)).toBe(true);
-  expect(_.isObject(yamlData)).toBe(true);
-});
+test.each(paths)('parse %s file', (__, file) => expect(_.isObject(parser(file))).toBe(true));
