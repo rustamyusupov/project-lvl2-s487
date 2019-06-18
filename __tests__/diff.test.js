6c9c39c087729ed348c7eaff0a562b5e3834e762
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-06-18 21:18:00 +0200

    test(tests): add nested tests for parser and diff

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -2,11 +2,17 @@ import diff from '../src';
 import { readFile } from '../src/parsers';
 
 const paths = ['json', 'yaml', 'ini'].map(ext => [
-  ext,
   `__tests__/__fixtures__/${ext}/before.${ext}`,
   `__tests__/__fixtures__/${ext}/after.${ext}`,
+  '__tests__/__fixtures__/result/result.txt',
 ]);
 
-test.each(paths)('compare %s files', (_, firstConfig, secondConfig) => {
-  expect(diff(firstConfig, secondConfig)).toBe(readFile('__tests__/__fixtures__/result/result.txt'));
+const nestedPaths = ['json', 'yaml', 'ini'].map(ext => [
+  `__tests__/__fixtures__/${ext}/before-nested.${ext}`,
+  `__tests__/__fixtures__/${ext}/after-nested.${ext}`,
+  '__tests__/__fixtures__/result/result-nested.txt',
+]);
+
+test.each(paths.concat(nestedPaths))('compare %s files', (firstConfig, secondConfig, result) => {
+  expect(diff(firstConfig, secondConfig)).toBe(readFile(result));
 });
--- __tests__/parsers.test.js
+++ __tests__/parsers.test.js
@@ -3,5 +3,6 @@ import _ from 'lodash/fp';
 import parser from '../src/parsers';
 
 const paths = ['json', 'yaml', 'ini'].map(ext => [ext, `__tests__/__fixtures__/${ext}/before.${ext}`]);
+const nestedPaths = ['json', 'yaml', 'ini'].map(ext => [ext, `__tests__/__fixtures__/${ext}/after-nested.${ext}`]);
 
-test.each(paths)('parse %s file', (__, file) => expect(_.isObject(parser(file))).toBe(true));
+test.each(paths.concat(nestedPaths))('parse %s file', (__, file) => expect(_.isObject(parser(file))).toBe(true));
