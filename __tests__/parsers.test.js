54a428236f48444735155a66f3be7d7ee0b82847
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-06-26 20:41:39 +0200

    test(tests): update persers

--- __tests__/parsers.test.js
+++ __tests__/parsers.test.js
@@ -1,8 +1,9 @@
 import _ from 'lodash/fp';
 
+import { readFile } from '../src';
 import parser from '../src/parsers';
 
 const paths = ['json', 'yaml', 'ini'].map(ext => [ext, `__tests__/__fixtures__/${ext}/before.${ext}`]);
 const nestedPaths = ['json', 'yaml', 'ini'].map(ext => [ext, `__tests__/__fixtures__/${ext}/after-nested.${ext}`]);
 
-test.each(paths.concat(nestedPaths))('parse %s file', (__, file) => expect(_.isObject(parser(file))).toBe(true));
+test.each(paths.concat(nestedPaths))('parse %s file', (ext, file) => expect(_.isObject(parser(ext, readFile(file)))).toBe(true));
