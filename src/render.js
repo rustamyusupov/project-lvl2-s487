9a236b6992379188f1dc5e9d22132606bde4b8ce
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-06-26 22:00:14 +0200

    feat(render): format result

--- src/render.js
+++ src/render.js
@@ -1,20 +1,34 @@
 import _ from 'lodash/fp';
 
-const operations = {
-  none: ({ key, valueBefore }) => `${key}: ${valueBefore}`,
-  add: ({ key, valueAfter }) => `+ ${key}: ${valueAfter}`,
-  remove: ({ key, valueBefore }) => `- ${key}: ${valueBefore}`,
-  change: ({ key, valueBefore, valueAfter }) => [
-    operations.remove({ key, valueBefore }),
-    operations.add({ key, valueAfter }),
-  ],
-  object: ({ key, children }, render) => `${key}: ${render(children)}`,
+const defaultIndent = '  ';
+const increment = 1;
+const getIndents = depth => `${defaultIndent}${defaultIndent}`.repeat(depth);
+
+const stringify = (value, depth) => {
+  if (!_.isObject(value)) {
+    return value;
+  }
+
+  return `{\n${Object.keys(value)
+    .map(key => `${getIndents(depth + increment)}${key}: ${value[key]}`)
+    .join('\n')}\n${getIndents(depth)}}`;
 };
 
-const render = (data) => {
-  const result = data.map(node => operations[node.type](node, render));
+const operations = {
+  none: ({ key, valueBefore }, depth) => `${getIndents(depth + increment)}${key}: ${stringify(valueBefore, depth + increment)}`,
+  add: ({ key, valueAfter }, depth) => `${defaultIndent}${getIndents(depth)}+ ${key}: ${stringify(valueAfter, depth + increment)}`,
+  remove: ({ key, valueBefore }, depth) => `${defaultIndent}${getIndents(depth)}- ${key}: ${stringify(valueBefore, depth + increment)}`,
+  change: ({ key, valueBefore, valueAfter }, depth) => [
+    operations.remove({ key, valueBefore }, depth),
+    operations.add({ key, valueAfter }, depth),
+  ],
+  object: ({ key, children }, depth, render) => `${getIndents(depth + increment)}${key}: ${render(children, depth + increment)}`,
+};
 
-  return _.flatten(result).join('\n');
+const render = (data, depth = 0) => {
+  const result = data.map(node => operations[node.type](node, depth, render));
+
+  return `{\n${_.flatten(result).join('\n')}\n${getIndents(depth)}}`;
 };
 
 export default render;
