ad47593f20c755b2d7bd89aa3ea2b9172c5f2255
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-06-27 21:21:40 +0200

    test(tests): add test for plain formatter

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,20 +1,23 @@
-import compare, { readFile } from '../src';
+import diff, { readFile } from '../src';
 
 const paths = ['json', 'yaml', 'ini'].map(ext => [
   `__tests__/__fixtures__/${ext}/before.${ext}`,
   `__tests__/__fixtures__/${ext}/after.${ext}`,
   '__tests__/__fixtures__/result/result.txt',
+  '__tests__/__fixtures__/result/result-plain.txt',
 ]);
 
 const nestedPaths = ['json', 'yaml', 'ini'].map(ext => [
   `__tests__/__fixtures__/${ext}/before-nested.${ext}`,
   `__tests__/__fixtures__/${ext}/after-nested.${ext}`,
   '__tests__/__fixtures__/result/result-nested.txt',
+  '__tests__/__fixtures__/result/result-nested-plain.txt',
 ]);
 
-test.each(paths.concat(nestedPaths))('compare %s files', (firstConfig, secondConfig, result) => {
-  const diff = compare(firstConfig, secondConfig);
+test.each(paths.concat(nestedPaths))('compare %s files', (firstConfig, secondConfig, result, resultPlain) => {
   const resultData = readFile(result);
+  const resultPlainData = readFile(resultPlain);
 
-  expect(diff).toBe(resultData);
+  expect(diff(firstConfig, secondConfig, 'default')).toBe(resultData);
+  expect(diff(firstConfig, secondConfig, 'plain')).toBe(resultPlainData);
 });
