d8f8cb98e88334641d5137f93531f23f775240f1
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-07-03 21:35:48 +0200

    test(tests): refactor read file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,16 +2,6 @@ import fs from 'fs';
 
 import gendiff from '../src';
 
-const readFile = (filePath) => {
-  try {
-    return fs.readFileSync(filePath, 'utf8');
-  } catch (e) {
-    console.log('Error:', e.stack);
-  }
-
-  return null;
-};
-
 test.each([
   ['json', 'plain'],
   ['yml', 'plain'],
@@ -22,16 +12,9 @@ test.each([
   ['json', 'json'],
   ['yml', 'json'],
   ['ini', 'json'],
-])(
-  'compare %s files, format %s',
-  (ext, format) => {
-    const diff = gendiff(
-      `__tests__/__fixtures__/before.${ext}`,
-      `__tests__/__fixtures__/after.${ext}`,
-      format,
-    );
-    const result = readFile(`__tests__/__fixtures__/result-${format}.txt`);
+])('compare %s files, format %s', (ext, format) => {
+  const diff = gendiff(`__tests__/__fixtures__/before.${ext}`, `__tests__/__fixtures__/after.${ext}`, format);
+  const result = fs.readFileSync(`__tests__/__fixtures__/result-${format}.txt`, 'utf8');
 
-    expect(diff).toStrictEqual(result);
-  },
-);
+  expect(diff).toStrictEqual(result);
+});
