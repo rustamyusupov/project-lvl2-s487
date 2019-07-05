ec46b20778ca8f0e5392eb114a6f58e9602f4a29
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-07-05 21:31:12 +0200

    test(tests): rename default format to tree

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,15 +3,15 @@ import fs from 'fs';
 import gendiff from '../src';
 
 test.each([
-  ['json', 'plain'],
-  ['yml', 'plain'],
-  ['ini', 'plain'],
-  ['json', 'default'],
-  ['yml', 'default'],
-  ['ini', 'default'],
   ['json', 'json'],
-  ['yml', 'json'],
   ['ini', 'json'],
+  ['yml', 'json'],
+  ['json', 'plain'],
+  ['ini', 'plain'],
+  ['yml', 'plain'],
+  ['json', 'tree'],
+  ['ini', 'tree'],
+  ['yml', 'tree'],
 ])('compare %s files, format %s', (ext, format) => {
   const diff = gendiff(`__tests__/__fixtures__/before.${ext}`, `__tests__/__fixtures__/after.${ext}`, format);
   const result = fs.readFileSync(`__tests__/__fixtures__/result-${format}.txt`, 'utf8');
